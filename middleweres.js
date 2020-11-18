080a78c09f25aae273b9dd04233e55174ab2db57
Vinicius-Nunes <57628027+Vinicius02612@users.noreply.github.com>
2020-11-18 01:15:58 -0300

    Delete middleweres.js

--- middleweres.js
+++
@@ -1,9 +0,0 @@
-//configurando cors da aplicação.
-//configurando middles 
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
-module.exports = app =>{
-    app.use(bodyParser.json())
-    app.use(cors())
-}
